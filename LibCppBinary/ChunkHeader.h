88b394afa7940f30568b93849fa85a911bcca766
Stephen Bonar <contactme@stephenbonar.com>
2025-05-04 17:04:08 -0700

    Finished documenting ChunkHeader
    -Added doc comments to the header

--- LibCppBinary/ChunkHeader.h
+++ LibCppBinary/ChunkHeader.h
@@ -25,13 +25,26 @@
 
 namespace Binary
 {
+    /// @brief Represents the header of a binary data chunk.
     struct ChunkHeader : public DataStructure
     {
+        /// @brief The ID of the chunk.
         StringField id{ 4 };
+
+        /// @brief The size of the chunk's data.
         UInt32Field dataSize;
 
+        /// @brief Provides a vector of raw pointers to the structure's fields.
+        ///
+        /// This method is primarily intended for use by Binary::Stream and its
+        /// derivatives for reading the fields from and to those streams.
+        /// Access the fields directly rather than via this method.
+        ///
+        /// @return The vector containing the raw pointers for the field.
         std::vector<DataField*> Fields() override;
 
+        /// @brief Gets the total size of all fields in the structure.
+        /// @return The total size of the structure.
         size_t Size() const override;
     };
 }
