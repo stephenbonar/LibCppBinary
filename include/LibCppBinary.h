a9b8210d7e73072335df960a8081e368877fb1a2
Stephen Bonar <contactme@stephenbonar.com>
2025-05-17 12:33:30 -0700

    Added main header file for library

---
+++ include/LibCppBinary.h
@@ -0,0 +1,33 @@
+// LibCppBinary.h - The main header file for the LibCppBinary library.
+//
+// Copyright (C) 2025 Stephen Bonar
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http ://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef LIB_CPP_BINARY_H
+#define LIB_CPP_BINARY_H
+
+#include "ChunkHeader.h"
+#include "DataField.h"
+#include "DataStructure.h"
+#include "FieldEndianness.h"
+#include "FileMode.h"
+#include "FileStream.h"
+#include "IntField.h"
+#include "RawField.h"
+#include "RawFileStream.h"
+#include "Stream.h"
+#include "StringField.h"
+#include "StringFormat.h"
+
+#endif
